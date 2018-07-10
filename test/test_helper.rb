0620f6d4050fbae0bf09cda7096cda0988fbe880
Geoffroy Planquart <geoffroy@planquart.fr>
2018-07-10 10:56:01 +0200

    added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'webmock/minitest'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+end
+
 require "mocked_website"
 
 require "minitest/autorun"
